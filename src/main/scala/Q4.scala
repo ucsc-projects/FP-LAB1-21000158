d499eae8a8ce5ac0fcee3751ab8018929be0a878
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-06-13 09:28:38 +0530

    Update Q4

--- src/main/scala/Q4.scala
+++ src/main/scala/Q4.scala
@@ -3,7 +3,10 @@ object Q4 {
     var coverPrice = 24.95;
     var quantity = 60;
     var discountedPrice = coverPrice * 0.6;
-    var totShipping = 3 + (quantity - 50) * 0.75;
+    var totShipping = 3;
+    if (quantity>50){
+        totShipping+=(quantity - 50) * 0.75;
+    }
     var totPrice = discountedPrice * 60 + totShipping;
     println("Total price for 60 books : " + totPrice);
   }
