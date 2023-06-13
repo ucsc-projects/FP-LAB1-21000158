f80eacea89f99651704d89a1701d746d91953083
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-06-13 08:54:34 +0530

    Finish Q3

--- src/main/scala/Q2.scala
+++ src/main/scala/Q2.scala
@@ -1,8 +1,7 @@
 object Q2 {
   def main(args: Array[String]) {
     var tempC = 35;
-    var tempF = tempC*1.8+32;
-    println("Temperature in Fahrenheit : " + tempF);
+    var tempF = tempC * 1.8 + 32;
+    println("35C temperature in Fahrenheit : " + tempF);
   }
-
 }
---
+++ src/main/scala/Q3.scala
@@ -0,0 +1,9 @@
+import scala.math.Pi
+
+object Q3 {
+  def main(args: Array[String]) {
+    var radius = 5;
+    var volume = (4/3)*Pi * radius * radius*radius;
+    println("Area of a radius 5 sphere : " + volume);
+  }
+}
